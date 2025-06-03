d9460aeeaaf9fa737a214c66a5ac423fdcaeba3e
Jeremy Echols <jechols@uoregon.edu>
2025-06-03 11:05:42 -0700

    docker: don't lose the config on restarts!
    Fixes the issue where `config.inc.php` is gone, and then replaced, after
    a "stop" / "down" type of compose command. OJS was replacing the file
    with the default which caused the install UI to show up repeatedly.
    
    The config file only needs to have replacements done once (for now), but
    it must be re-linked into the app dir every startup since the symlink is
    just an in-container file.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,9 +4,10 @@ init() {
   if [ ! -e /var/local/config/config.inc.php ]; then
     /replace-vars.sh /config-template.ini /var/local/config/config.inc.php
     chown www-data /var/local/config/config.inc.php
-    su -s /bin/bash -c "ln -sf /var/local/config/config.inc.php /var/www/html/config.inc.php" - www-data
-    chmod -w /var/local/config/config.inc.php
   fi
+
+  su -s /bin/bash -c "ln -sf /var/local/config/config.inc.php /var/www/html/config.inc.php" - www-data
+  chmod -w /var/local/config/config.inc.php
 }
 
 # When user requests bash or sh, don't run the init function
