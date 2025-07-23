08616bc8b1976ab0b2a78d5eb4ea9b976dadb6dc
Jeremy Echols <jechols@uoregon.edu>
2025-07-23 13:05:37 -0700

    Run key generation as www-data user
    Fixes an error where the op cache is initially filled by root, which
    totally busts the directory's permissions

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,13 +6,14 @@ init() {
   if [ ! -e /var/local/config/config.inc.php ]; then
     /replace-vars.sh /config-template.ini /var/local/config/config.inc.php
     chown root:www-data /var/local/config/config.inc.php
-    chmod 640 /var/local/config/config.inc.php
   fi
 
   su -s /bin/bash -c "ln -sf /var/local/config/config.inc.php /var/www/html/config.inc.php" - www-data
 
   # Set up the app key: the OJS tool won't replace it if it's already been set
-  php ./lib/pkp/tools/appKey.php generate
+  chmod 660 /var/local/config/config.inc.php
+  su -s /bin/bash -c "cd /var/www/html && php ./lib/pkp/tools/appKey.php generate" - www-data
+  chmod 440 /var/local/config/config.inc.php
 }
 
 # When user requests bash or sh, don't run the init function
