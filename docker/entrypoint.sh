b72ab15229ce3177dcd23cab085a2db351380f88
Jeremy Echols <jechols@uoregon.edu>
2025-08-04 09:05:18 -0700

    Use a var for conf file location in entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
+conffile=/var/local/config/config.inc.php
 init() {
   wait_for_database
 
-  if [ ! -e /var/local/config/config.inc.php ]; then
-    /replace-vars.sh /config-template.ini /var/local/config/config.inc.php
-    chown root:www-data /var/local/config/config.inc.php
+  if [ ! -e $conffile ]; then
+    /replace-vars.sh /config-template.ini $conffile
+    chown root:www-data $conffile
   fi
 
-  su -s /bin/bash -c "ln -sf /var/local/config/config.inc.php /var/www/html/config.inc.php" - www-data
+  su -s /bin/bash -c "ln -sf $conffile /var/www/html/config.inc.php" - www-data
 
   # Set up the app key: the OJS tool won't replace it if it's already been set
-  chmod 660 /var/local/config/config.inc.php
+  chmod 660 $conffile
   su -s /bin/bash -c "cd /var/www/html && php ./lib/pkp/tools/appKey.php generate" - www-data
-  chmod 440 /var/local/config/config.inc.php
+  chmod 440 $conffile
 }
 
 # When user requests bash or sh, don't run the init function
